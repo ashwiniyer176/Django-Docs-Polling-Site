d7e003843b18e5a88abc3d581045b659142c6af6
Ashwin Iyer <ashwiniyer1706@gmail.com>
2021-07-18 18:46:54 +0530

    Voting feature added

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django import template
-from django.http.response import Http404
-from django.shortcuts import render
+from django.http.response import Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import context, loader
+from django.urls import reverse
 # Create your views here.
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -25,8 +26,20 @@ def detail(request, questionID):
 
 def results(request, questionID):
     response = "You're looking at results of question %s"
-    return HttpResponse(response % questionID)
+    question = get_object_or_404(Question, pk=questionID)
+    return render(request, 'polls/results.html', context={'question': question})
 
 
 def vote(request, questionID):
-    return HttpResponse("You're voting on question %s." % questionID)
+    question = get_object_or_404(Question, pk=questionID)
+    try:
+        selectedChoice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', context={
+            'question': question,
+            'error': "You did not select a choice"
+        })
+    else:
+        selectedChoice.votes += 1
+        selectedChoice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
