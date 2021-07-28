01688c95d4e65eb00575f103ee49c4bd5f95f6fb
Ashwin Iyer <ashwiniyer1706@gmail.com>
2021-07-28 13:05:49 +0530

    Convert to class based views

--- polls/views.py
+++ polls/views.py
@@ -1,33 +1,28 @@
-from django import template
 from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import context, loader
 from django.urls import reverse
+from django.views import generic
 # Create your views here.
 from .models import Question, Choice
 
 
-def index(request):
-    questionsList = Question.objects.order_by('-publishedDate')[:5]
-    context = {
-        'questionsList': questionsList
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'questionsList'
+
+    def get_queryset(self):
+        '''Return the last five published questions'''
+        return Question.objects.order_by('-publishedDate')[:5]
 
 
-def detail(request, questionID):
-    try:
-        question = Question.objects.get(pk=questionID)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, questionID):
-    response = "You're looking at results of question %s"
-    question = get_object_or_404(Question, pk=questionID)
-    return render(request, 'polls/results.html', context={'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, questionID):
